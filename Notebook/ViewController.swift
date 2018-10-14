30cdcd9f84dffce7aea5155ff5fdbd4d458f60e7
Ivan Mavrazas <mavrazas@gmail.com>
2018-10-14 10:31:38 +0200

    Initial Commit

---
+++ Notebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notebook
+//
+//  Created by Ivan Mavrazas on 10/14/18.
+//  Copyright © 2018 Ivan Mavrazas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
